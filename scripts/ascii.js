4c4cbb1d2a6e3d7a460de6dc5a357839083a77fb
xbeatzsec <hernanifurtado@gmail.com>
2021-12-19 17:20:35 +0000

    updates

--- scripts/ascii.js
+++ scripts/ascii.js
@@ -14,12 +14,11 @@ $(function(){
 
       "<span style='color:#009966;'><pre>Testing</pre>",
 
-      "<span style='color:#009966;'><pre>testing</pre>",
+      "<span style='color:#009966;'><pre>Type:\".\" for some open sourcing</pre>",
 
 "<span style='color:#009966;'><pre>Hernani, 21 yo, Pentester</pre>",
 
       "<span style='color:#009966;'><pre>Type: \"nani\" and you see magic</pre>",
-          "<span style='color:#009966;'><pre>+---[RSA 4096]----+<br>|#E*....          |<br>|%#++n .           |<br>|@*O.=a            |<br>|O*.B . .n        |<br>|*o. o . S-i       |<br>|oo . . .         |<br>|o .   .          |<br>|.                |<br>|                 |<br>+----[SHA256]-----+<br></pre>"
       ],
       contentType: 'html',
       typeSpeed: 0,
